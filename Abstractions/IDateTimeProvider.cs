881860c6d4206eb58d04a61b5b9c7ed20360448f
Khánh Hưng <khanhhungzm@gmail.com>
2026-04-13 19:54:29 +0700

    feat: implement base datetime abstractions and system provider

---
+++ Abstractions/IDateTimeProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Platform.BuildingBlocks.Abstractions
+{
+    public interface IDateTimeProvider
+    {
+        DateTime UtcNow { get; }
+    }
+}
---
+++ DateTimes/SystemDateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using Platform.BuildingBlocks.Abstractions;
+
+namespace Platform.BuildingBlocks.DateTimes
+{
+    public class SystemDateTimeProvider : IDateTimeProvider
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
