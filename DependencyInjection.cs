7e8edd9b0d27bd53e2eb973355c5876aaff44c89
Khánh Hưng <khanhhungzm@gmail.com>
2026-04-14 00:11:20 +0700

    feat(buildingblocks): add dependency injection registration

---
+++ DependencyInjection.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using Platform.BuildingBlocks.Abstractions;
+using Platform.BuildingBlocks.DateTimes;
+
+namespace Platform.BuildingBlocks;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBuildingBlocks(this IServiceCollection services)
+    {
+        services.AddSingleton<IDateTimeProvider, SystemDateTimeProvider>();
+        return services;
+    }
+}
